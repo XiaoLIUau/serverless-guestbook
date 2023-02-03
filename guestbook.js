cd06d784a8e47eb84541b6bea05ba82e5ae41e85
XiaoLIUau <86995584+XiaoLIUau@users.noreply.github.com>
2023-02-03 11:47:19 +1000

    Update guestbook.js
    Update apiUrl to my endpoint in ibm cloud

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/cf6ddc68-e7a0-4595-bee8-b1d94fbc3fd4/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
